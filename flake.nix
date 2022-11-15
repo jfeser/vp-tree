053a794285b525aa95389fbd1152802c7596498a
Jack Feser <feser@mit.edu>
2022-11-14 19:24:14 -0500

    use overlay in flake

--- flake.nix
+++ flake.nix
@@ -1,27 +1,32 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, flake-utils, nixpkgs }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        ocamlPkgs = pkgs.ocaml-ng.ocamlPackages;
-        defaultPackage = ocamlPkgs.buildDunePackage rec {
-          pname = "vpt";
-          version = "5.0.0";
-          useDune3 = true;
-          src = ./.;
-          nativeBuildInputs =
-            [ ocamlPkgs.base ocamlPkgs.iter ocamlPkgs.ppx_jane ];
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay.${system} ];
         };
       in {
-        defaultPackage = defaultPackage;
+        overlay = self: super: {
+          ocamlPackages = super.ocamlPackages.overrideScope' (self: super: {
+            vpt = super.buildDunePackage {
+              pname = "vpt";
+              version = "5.0.0";
+              duneVersion = "3";
+              src = ./.;
+              nativeBuildInputs = [ self.base self.iter self.ppx_jane ];
+            };
+          });
+        };
+        defaultPackage = pkgs.ocamlPackages.vpt;
         devShell = pkgs.mkShell {
           nativeBuildInputs =
             [ pkgs.ocamlformat pkgs.opam pkgs.ocamlPackages.ocaml-lsp ];
-          inputsFrom = [ defaultPackage ];
+          inputsFrom = [ pkgs.ocamlPackages.vpt ];
         };
       });
 }
